79b0c58cda54638a1076305f5699e7fe9112d4a2
Bhuvanesh-26 <108187416+Bhuvanesh-26@users.noreply.github.com>
2024-11-20 12:16:21 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        int x=5;
+        int y=6;
+        System.out.println(x+y)
     }
 }
